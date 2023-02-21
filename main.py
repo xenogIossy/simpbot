af0d4e10f6b259426d25af9d9cafdb2bfb01a143
alexis <mercuryreads@gmail.com>
2023-02-21 20:46:03 +0100

    a

---
+++ main.py
@@ -0,0 +1,25 @@
+import discord
+import config
+from discord.ext import commands
+
+token = config.token
+intents = discord.Intents().all()
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+#client = discord.Client(intents=discord.Intents.default())
+
+@bot.event
+async def on_ready():
+    print(f'Bot connected as {bot.user}')
+    await bot.change_presence(activity=discord.Game(name="Python"))
+
+@bot.event
+async def on_message(message):
+    print("Message from {0.author}: {0.content}".format(message))
+    await bot.process_commands(message)
+
+@bot.command()
+async def ayo(ctx, member : commands.MemberConverter):
+    await ctx.send(f"{member.mention} is SIMPING!")
+
+bot.run(token)
